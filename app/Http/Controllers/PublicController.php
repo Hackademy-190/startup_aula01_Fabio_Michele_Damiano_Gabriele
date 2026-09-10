3a1bab4e79a0b2b6a802019c8d23f206a69935e6
Damiano <damianori18@gmail.com>
2026-09-10 19:02:28 +0200

    section chi-siamo

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -22,13 +22,116 @@ class PublicController extends Controller
 
         return view('blog-show', compact('post'));
     }
-    
+
+    private function posts(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor 1',
+                'date' => '10 settembre 2026',
+                'content' => 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Lorem ipsum dolor 2',
+                'date' => '3 settembre 2026',
+                'content' => 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Lorem ipsum dolor 3',
+                'date' => '27 agosto 2026',
+                'content' => 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            ],
+        ];
+    }
+
+    public function chiSiamo()
+    {
+
+        $team = [
+            [
+                'id' => 1,
+                'nome' => 'Michele Giovanni Noviello',
+                'ruolo' => 'CEO',
+                'descrizione' => 'Michele è il fondatore e CEO della nostra azienda. Con oltre 20 anni di esperienza nel settore, guida il team con passione e dedizione.',
+                'immagine' => 'media/michele.jpg',
+            ],
+            [
+                'id' => 2,
+                'nome' => 'Fabio Spanò',
+                'ruolo' => 'Operations Manager',
+                'descrizione' => "Fabio è il nostro Operations Manager. È responsabile della gestione operativa e dell'efficienza dei processi aziendali.",
+                'immagine' => 'media/fabio.jpg',
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Damiano Ricci',
+                'ruolo' => 'Ingegnere del Software',
+                'descrizione' => "Damiano è l'ingegnere del software della nostra azienda. Si occupa dello sviluppo e della manutenzione delle nostre applicazioni.",
+                'immagine' => 'media/damiano.jpg',
+            ],
+            [
+                'id' => 4,
+                'nome' => 'Gabriele Nunnari',
+                'ruolo' => 'Art Director',
+                'descrizione' => 'Gabriele gestisce le strategie di marketing e comunicazione, assicurando che il nostro messaggio raggiunga il pubblico giusto.',
+                'immagine' => 'media/gabriele.jpg',
+            ],
+        ];
+
+        return view('chi-siamo', ['team' => $team]);
+    }
+
+    public function chiSiamoShow($id)
+    {
+
+        $team = [
+            [
+                'id' => 1,
+                'nome' => 'Michele Giovanni Noviello',
+                'ruolo' => 'CEO',
+                'descrizione' => 'Michele è il fondatore e CEO della nostra azienda. Con oltre 20 anni di esperienza nel settore, guida il team con passione e dedizione.',
+                'immagine' => 'media/michele.jpg',
+            ],
+            [
+                'id' => 2,
+                'nome' => 'Fabio Spanò',
+                'ruolo' => 'Operations Manager',
+                'descrizione' => "Fabio è il nostro Operations Manager. È responsabile della gestione operativa e dell'efficienza dei processi aziendali.",
+                'immagine' => 'media/fabio.jpg',
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Damiano Ricci',
+                'ruolo' => 'Ingegnere del Software',
+                'descrizione' => "Damiano è l'ingegnere del software della nostra azienda. Si occupa dello sviluppo e della manutenzione delle nostre applicazioni.",
+                'immagine' => 'media/damiano.jpg',
+            ],
+            [
+                'id' => 4,
+                'nome' => 'Gabriele Nunnari',
+                'ruolo' => 'Art Director',
+                'descrizione' => 'Gabriele gestisce le strategie di marketing e comunicazione, assicurando che il nostro messaggio raggiunga il pubblico giusto.',
+                'immagine' => 'media/gabriele.jpg',
+            ],
+        ];
+
+        foreach ($team as $member) {
+            if ($id == $member['id']) {
+
+                return view('chi-siamo-show', ['member' => $member]);
+            }
+        }
+
+        return view('chi-siamo-show', ['team' => $team]);
+    }
 
     public function contatti()
     {
         return view('contatti');
     }
-
 
     public function contattiSubmit(Request $request)
     {
