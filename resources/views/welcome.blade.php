4672390d8f279ff44d5faf6334eb0f6d63e40ead
Gabriele Nunnari <nunnarigabriele@gmail.com>
2026-09-10 13:52:41 +0200

    Navbar-footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,12 @@
 <x-layout>
     <x-navbar />
-    <h1>ciao</h1>
+
+  {{--  bg di prova --}}
+    <div class="vh-100 text-center py-5" style="background-color:#20282c">
+
+        <h1>ciao</h1>
+
+    </div>
+
+    <x-footer />
 </x-layout>
