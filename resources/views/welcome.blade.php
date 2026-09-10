9e0d325ededf18d968192f42ddb8bfc6b06159b5
Gabriele Nunnari <nunnarigabriele@gmail.com>
2026-09-10 17:12:18 +0200

    navbar fissa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
+
 <x-layout>
-    <header class="container-fluid flex-grow-1 d-flex align-items-center justify-content-center">
-        <div class="text-center">
-            <h1 class="fs-1">Sviluppo web affidabile per il tuo progetto</h1>
-            <p>Realizziamo siti, applicazioni web e soluzioni digitali solide, performanti e facili da mantenere.</p>
+    <header class="container-fluid flex-grow-1 d-flex align-items-center justify-content-center py-5">
+        <div class="text-center col-11 col-md-9 col-lg-7">
+            <h1 class="fs-1 mb-3">Sviluppo web affidabile per il tuo progetto</h1>
+            <p class="fs-5 mb-4" style="color: var(--text-muted);"> Realizziamo siti, applicazioni web e soluzioni digitali solide, performanti e facili da mantenere.</p>
             <a href="{{ route('blog') }}" class="btn-primario">Scopri i nostri progetti</a>
         </div>
     </header>
