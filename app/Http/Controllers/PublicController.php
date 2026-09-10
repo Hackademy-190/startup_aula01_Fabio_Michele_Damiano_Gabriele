b4da0046ed748dc87e09387733d394811498dcd4
fabiospanoo <fabiospano19012006@gmail.com>
2026-09-10 12:24:19 +0200

    navbar stable

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    //
+    public function homepage(){
+        return view("welcome");
+    }
+    
+    public function progetti(){
+        return view("progetti");
+    }
+    
+    public function contatti(){
+        return view("contatti");
+    }
+    
+    public function chiSiamo(){
+        return view("chi-siamo");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PublicController::class, 'homepage'])->name('welcome');
+
+Route::get('/contatti', [PublicController::class, 'contatti'])->name('contatti');
+
+Route::get('/progetti', [PublicController::class, 'progetti'])->name('progetti');
+
+Route::get('/chi-siamo', [PublicController::class, 'chiSiamo'])->name('chi-siamo');
