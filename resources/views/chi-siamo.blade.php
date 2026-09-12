75e920d1a7e73749b82ba34f5bbe290cc3d818c6
Damiano <damianori18@gmail.com>
2026-09-12 18:49:35 +0200

    before merge

--- resources/views/chi-siamo.blade.php
+++ resources/views/chi-siamo.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <!--Il nostro team-->
-    <section class="container py-5 text-center">
+    <section class="container py-5 text-center vh-100 d-flex align-items-center">
 
         <div class="row text-center g-4">
 
@@ -8,8 +8,9 @@
                 <h2 class="section-title">Chi Siamo</h2>
             </div>
 
-            @foreach ($team as $member)
+            {{-- @foreach ($team as $member)
                 <div class="col-6 col-md-3 pt-5">
+                    
                     <img src="{{ asset($member['immagine']) }}"
                         class="rounded-circle p-1 bg-white border border-2 shadow-sm mb-3"
                         style="width: 140px; height: 140px; object-fit: cover;" alt="">
@@ -18,7 +19,9 @@
                     <p>{{ $member['ruolo'] }}</p>
                     <a href="{{route('chi-siamo-show', ['id' => $member['id']])}}" class="btn-primario">Visualizza dettagli</a>
                 </div>
-            @endforeach
+            @endforeach --}}
+
+
 
         </div>
 
